b9ffca22f83672520483c6b70931a63c351b2f3e
yo <alv.monsalve@duocuc.cl>
2025-05-26 20:46:59 -0400

    producto

--- src/main/java/com/ecomarket/reabastecimiento/controller/ProductoController.java
+++ src/main/java/com/ecomarket/reabastecimiento/controller/ProductoController.java
@@ -13,6 +13,9 @@ import com.ecomarket.reabastecimiento.service.ProductoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -42,5 +45,15 @@ public class ProductoController {
         }    
     }
     
+    @PutMapping("actualizar/{id_producto}")
+    public ResponseEntity<Producto> putProducto(@PathVariable int id_producto, @RequestBody Producto producto) {
+        Producto prod = productoService.updateById(id_producto, producto);
+        if (prod != null) {
+            return new ResponseEntity<>(prod, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
+    
 }
